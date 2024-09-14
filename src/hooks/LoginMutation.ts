aaaf3262f10062f265fb7f7292b3c278c69c2f75
ahmedhamila.stage <77456894+ahmedhamila@users.noreply.github.com>
2024-09-14 11:26:52 +0100

    update middleware

--- src/hooks/LoginMutation.ts
+++ src/hooks/LoginMutation.ts
@@ -1,23 +1,23 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useRouter } from "next/navigation"
 
-import { useRouter } from 'next/navigation'
-import { getTokens, TokenRequest } from '@/services/authService'
+import { getTokens, TokenRequest } from "@/services/authService"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 export const useLogin = (key: string) => {
-  const queryClient = useQueryClient()
-  const router = useRouter()
-  return useMutation({
-    mutationFn: async (data: TokenRequest) => {
-      const response = await getTokens(data)
-      return response
-    },
-    onError: (error: any) => {
-      alert("error")
-    },
-    onSuccess: async (result: any) => {
-      alert("Successful Login")
-      router.replace(`/dashboard` as any)
-      await queryClient.invalidateQueries({ queryKey: [key] })
-    }
-  })
+	const queryClient = useQueryClient()
+	const router = useRouter()
+	return useMutation({
+		mutationFn: async (data: TokenRequest) => {
+			const response = await getTokens(data)
+			return response
+		},
+		onError: (error: any) => {
+			console.log("error")
+		},
+		onSuccess: async (result: any) => {
+			console.log("Successful Login")
+			router.replace(`/dashboard` as any)
+			await queryClient.invalidateQueries({ queryKey: [key] })
+		}
+	})
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,27 @@
 import { NextRequest, NextResponse } from "next/server"
 
-import { getCookieSS } from "./helpers/cookieHelpersSS"
-
 // 1. Specify protected and public routes
 const protectedRoutes = ["/dashboard"]
 const publicRoutes = ["/login"]
 
-export default async function middleware(req: NextRequest) {
-	// 2. Check if the current route is protected or public
+export default function middleware(req: NextRequest) {
 	const path = req.nextUrl.pathname
+
+	// 2. Check if the current route is protected or public
 	const isProtectedRoute = protectedRoutes.includes(path)
 	const isPublicRoute = publicRoutes.includes(path)
 
-	// 3. Decrypt the session from the cookie
-	const cookie = getCookieSS("accessToken")
-
-	// 5. Redirect to /login if the user is not authenticated
+	// 3. Fetch the cookie from the request
+	const cookie = req.cookies.get("accessToken") || null
+	console.log("Path:", path)
+	console.log("Cookie:", cookie)
+	// 4. Redirect to /login if the user is not authenticated and trying to access a protected route
 	if (isProtectedRoute && !cookie) {
 		return NextResponse.redirect(new URL("/login", req.nextUrl))
 	}
 
-	// 6. Redirect to /dashboard if the user is authenticated
-	if (
-		isPublicRoute &&
-		cookie &&
-		!req.nextUrl.pathname.startsWith("/dashboard")
-	) {
+	// 5. Redirect to /dashboard if the user is authenticated and trying to access a public route
+	if (isPublicRoute && cookie) {
 		return NextResponse.redirect(new URL("/dashboard", req.nextUrl))
 	}
 
